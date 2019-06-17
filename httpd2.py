56901b2cd413950f65a8316cb5d7417603ddf4c3
cold <misterespia666@yandex.ru>
2019-06-17 07:15:26 -0400

    extra empty lines removed

--- httpd2.py
+++ httpd2.py
@@ -9,18 +9,12 @@ from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import sessionmaker
 
-
-
-
 MAX_CONTENT_LEN=2000
 
 app = Flask(__name__)
-
-
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
 
 from models import *
 
@@ -145,9 +139,6 @@ def main():
     db.session.close()
     answer = '{"date":"%s","interval":%d,"messages":%s}' % (time.strftime("%Y-%m-%d %H:%M:%S"),ctrl.interval, json.dumps(answer))
     return jsonify(answer)
-
-        
-
 
 if __name__ == '__main__':
     app.run()
