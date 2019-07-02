c4fc2fdd185d600a2295dbae396e12bd34876ed7
tornado67 <misterespia666@yandex.ru>
2019-07-02 10:54:36 -0400

    some logging added

--- httpd2.py
+++ httpd2.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 import json
 import datetime 
@@ -51,6 +52,7 @@ def main():
         jsn =  request.get_json()
     except ValueError:
         raise  BadRequest('Malformed json', status_code=400)
+    print ("received request: " + str(jsn), file=sys.stderr)
     sn = jsn.get('sn')
     type = jsn.get('type')
     ctrl = db.session.query(Controller).filter(Controller.serial==sn, Controller.type==type).first()
@@ -60,21 +62,21 @@ def main():
         req_id = msg_json.get('id')
         if operation == None:
             if msg_json.get('success')== 1:
-                print("ANSWER TO %d FROM CONTROLLER %d" % (req_id,sn) )
+                print("ANSWER TO %d FROM CONTROLLER %d" % (req_id,sn),file=sys.stderr )
                 db.session.query(Task).filter(id==req_id).delete()  
                 db.session.commit()
             else:
-                print("UNKNOWN ANSWER:\n%s" % (msg_json))
+                print("UNKNOWN ANSWER:\n%s" % (msg_json),file=sys.stderr)
 
         elif operation == 'power_on':
-            print('CONTROLLER %d POWER ON' % sn)
+            print('CONTROLLER %d POWER ON' % sn,file=sys.stderr)
             fw = msg_json.get('fw')
             conn_fw = msg_json.get('conn_fw')
             active = msg_json.get('active')
             mode = msg_json.get('mode')
             if ctrl == None:
 
-                print('UNKNOWN CONTROLLER ADD TO BASE')
+                print('UNKNOWN CONTROLLER ADD TO BASE',file=sys.stderr)
                 controller = Controller(serial=sn,type=type, fw=fw, conn_fw=conn_fw,active=mode, last_conn=int(time.time())  )
                 db.session.add(controller)
                 db.session.commit()
@@ -94,7 +96,7 @@ def main():
                  
         elif operation == "ping":
 
-            print("PING FROM CONTROLLER %d" % sn )
+            print("PING FROM CONTROLLER %d" % sn , file=sys.stderr)
             active = msg_json.get('active')
             mode = msg_json.get('mode')
             controller = db.session.query(Controller).filter(Controller.serial==sn, Controller.type==type).first()
@@ -109,12 +111,12 @@ def main():
             
             card = msg_json.get('card')
             reader = msg_json.get('reader')
-            print("CHECK ACCESS FROM CONTROLLER %d [%s on %d]" % (sn,card,reader))
+            print("CHECK ACCESS FROM CONTROLLER %d [%s on %d]" % (sn,card,reader),file=sys.stderr)
             granted = 1
             answer.append(json.loads('{"id":%d,"operation":"check_access","granted":%d}' % (req_id,granted)))
 
         elif operation == "events":
-            print("EVENTS FROM CONTROLLER %d" % sn )
+            print("EVENTS FROM CONTROLLER %d" % sn, file=sys.stderr)
             event_cnt = 0
             for event in  msg_json.get('events'):
                 event_cnt += 1
@@ -123,11 +125,11 @@ def main():
                 db.session.add(e)
             db.session.commit()
 
-            print("EVENT_SUCCESS: %d" % event_cnt)
+            print("EVENT_SUCCESS: %d" % event_cnt,file=sys.stderr)
             answer.append(json.loads('{"id":%d,"operation":"events","events_success":%d}' % (req_id,event_cnt)))
 
         else:
-            print('UNKNOWN OERATION')
+            print('UNKNOWN OERATION',file=sys.stderr)
     for task_jsn in db.session.query(Task.json).filter(Task.serial==sn, Task.type==type):
         if (len(json.dumps(answer))+len(task_jsn['json'])) > 1500:
             break
@@ -136,6 +138,7 @@ def main():
         answer.append(task)
     answer = '{"date":"%s","interval":%d,"messages":%s}' % (time.strftime("%Y-%m-%d %H:%M:%S"),ctrl.interval, json.dumps(answer))        
     db.session.close()
+    print ("reponse:"+ answer,file=sys.stderr)
     return answer
 
 if __name__ == '__main__':
