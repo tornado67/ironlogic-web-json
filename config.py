02070eeed618f54ddd94a3cb1a21ea59138585b1
cold <misterespia666@yandex.ru>
2019-08-27 13:31:08 +0300

    Develop (#11)
    * fix (#2)
    
    * dev files removed from master branch
    
    * Develop (#1)
    
    * error 500 on ping fix
    
    * dependencies fix
    
    * constructor fix for Event
    
    * extra empty lines removed
    
    * Create README.md
    
    * Update README.md
    
    * Update README.md
    
    * example env added. Instance is not bound to a session error fixed
    
    * some 500 erros  fixed
    
    * power_on 500 fixed
    
    * HOTFIX: 500 on controller power_on
    
    * malformed json response fix
    
    * Controller's constructor fix
    
    * brackets fix
    
    * some logging added
    
    * Time fix (#4)
    
    * 'readable_time' column added
    
    * style fixes
    
    * 'readable_time' column added
    
    * readable_time default value added.
    
    * Time fix (#5)
    
    * 'readable_time' column added
    
    * style fixes
    
    * 'readable_time' column added
    
    * readable_time default value added.
    
    * migrations fix
    
    * Time fix (#7)
    
    * 'readable_time' column added
    
    * style fixes
    
    * 'readable_time' column added
    
    * readable_time default value added.
    
    * migrations fix
    
    * time format fix
    
    * card changed type to String(128) (VARCHAR(128), timezone fixed
    
    * ttimezone fixed
    
    * ttimezone fixed
    
    * Time fix (#8)
    
    * 'readable_time' column added
    
    * style fixes
    
    * 'readable_time' column added
    
    * readable_time default value added.
    
    * migrations fix
    
    * time format fix
    
    * card changed type to String(128) (VARCHAR(128), timezone fixed
    
    * ttimezone fixed
    
    * ttimezone fixed
    
    * timezone added
    
    * yet another timezone fix
    
    * timezone sensetivity added
    
    * pytz added to requirements
    
    * identation fix
    
    * Json fix (#10)
    
    * minor fixed: length check removed.
    
    * miror fixes: request type check removed
    
    * temp: fix
    
    * json formation refactored
    
    * json formation refactored.
    temp output left for testing purposes.
    
    * json formation refactored.
    temp output left for testing purposes.
    
    * json formation refactored. temp output removed.
    
    * style fixes
    
    * identation & style fixes
    
    * style fixes
    
    * test commit
    
    * db.session maintenance fix
    
    * indentation fix
    
    * session scope fix
    
    * session handling
    
    * event count refactored
    
    * gunicorn added to requirements
    
    * gunicorn deploy added
    
    * helm chart created

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 class Config(object):
     DEBUG = False
     TZ = 'Europe/Moscow'
@@ -10,20 +9,16 @@ class Config(object):
     SECRET_KEY = 'this-really-needs-to-be-changed'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
-
 class ProductionConfig(Config):
     DEBUG = False
-
 
 class StagingConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
 
-
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
-
 
 class TestingConfig(Config):
     TESTING = True
--- httpd2.py
+++ httpd2.py
@@ -13,8 +13,6 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import sessionmaker
 from pytz import timezone
 
-MAX_CONTENT_LEN=2000
-
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -45,12 +43,7 @@ def handle_bad_request(error):
 
 @app.route('/', methods = ['POST'])
 def main():
-    if request.method != 'POST':
-        raise BadRequest('Not implemented', status_code=501)
-    answer = []
-   
-    if int(request.headers.get('Content-Length') or 0) > MAX_CONTENT_LEN:
-        raise  BadRequest('Too long', status_code=400)
+    messages = []
     try:
         jsn =  request.get_json()
     except ValueError:
@@ -58,89 +51,117 @@ def main():
     print ("received request: " + str(jsn), file=sys.stderr)
     sn = jsn.get('sn')
     type = jsn.get('type')
-    ctrl = db.session.query(Controller).filter(Controller.serial==sn, Controller.type==type).first()
+    try:
+        ctrl = db.session.query(Controller).filter(Controller.serial==sn, Controller.type==type).first()
     
-    for msg_json in jsn['messages']:
-        operation = msg_json.get('operation')
-        req_id = msg_json.get('id')
-        if operation == None:
-            if msg_json.get('success')== 1:
-                print("ANSWER TO %d FROM CONTROLLER %d" % (req_id,sn),file=sys.stderr )
-                db.session.query(Task).filter(id==req_id).delete()  
-                db.session.commit()
-            else:
-                print("UNKNOWN ANSWER:\n%s" % (msg_json),file=sys.stderr)
+        for msg_json in jsn['messages']:
+            operation = msg_json.get('operation')
+            req_id = msg_json.get('id')
+            if operation == None:
+               if msg_json.get('success')== 1:
+                    print("ANSWER TO %d FROM CONTROLLER %d" % (req_id,sn),file=sys.stderr )
+                    db.session.query(Task).filter(id==req_id).delete()
+                    db.session.commit()
+               else:
+                    print("UNKNOWN ANSWER:\n%s" % (msg_json),file=sys.stderr)
 
-        elif operation == 'power_on':
-            print('CONTROLLER %d POWER ON' % sn,file=sys.stderr)
-            fw = msg_json.get('fw')
-            conn_fw = msg_json.get('conn_fw')
-            active = msg_json.get('active')
-            mode = msg_json.get('mode')
-            if ctrl == None:
+            elif operation == 'power_on':
+                print('CONTROLLER %d POWER ON' % sn,file=sys.stderr)
+                fw = msg_json.get('fw')
+                conn_fw = msg_json.get('conn_fw')
+                active = msg_json.get('active')
+                mode = msg_json.get('mode')
+                if ctrl == None:
+                    print('UNKNOWN CONTROLLER ADD TO BASE',file=sys.stderr)
+                    controller = Controller(serial=sn,type=type, fw=fw, conn_fw=conn_fw,active=mode, last_conn=int(time.time())  )
+                    db.session.add(controller)
+                    db.session.commit()
+                    ctrl =  db.session.query(Controller).filter(Controller.serial==sn, Controller.type==type).first()
 
-                print('UNKNOWN CONTROLLER ADD TO BASE',file=sys.stderr)
-                controller = Controller(serial=sn,type=type, fw=fw, conn_fw=conn_fw,active=mode, last_conn=int(time.time())  )
-                db.session.add(controller)
-                db.session.commit()
-                ctrl =  db.session.query(Controller).filter(Controller.serial==sn, Controller.type==type).first()
+                else:
+                    controller = db.session.query(Controller).filter(Controller.serial==sn, Controller.type==type).first()
+                    controller.fw = fw
+                    controller.conn_fw = conn_fw
+                    controller.mode = mode
+                    controller.last_conn = int(time.time()),
+                    db.session.commit()
 
-            else:
-                controller = db.session.query(Controller).filter(Controller.serial==sn, Controller.type==type).first()
-                controller.fw = fw
-                controller.conn_fw = conn_fw
-                controller.mode = mode
-                controller.last_conn = int(time.time()),
-                db.session.commit()
-
-            if active != ctrl.active:
-                answer.append(json.loads('{"id":0,"operation":"set_active","active": %d,"online": 0}' % ctrl.active))
+                if active != ctrl.active:
+                    messages.append(
+                        dict (
+                            id = '0',
+                            operation = 'set_active',
+                            active = ctrl.active,
+                            online = '0'
+                         )
+                     )
                  
-        elif operation == "ping":
-            print("PING FROM CONTROLLER %d" % sn , file=sys.stderr)
-            active = msg_json.get('active')
-            mode = msg_json.get('mode')
-            controller = db.session.query(Controller).filter(Controller.serial==sn, Controller.type==type).first()
-            controller.mode = mode
-            controller.last_conn = int(time.time())
-            db.session.commit()  
+            elif operation == "ping":
+                print("PING FROM CONTROLLER %d" % sn , file=sys.stderr)
+                active = msg_json.get('active')
+                mode = msg_json.get('mode')
+                controller = db.session.query(Controller).filter(Controller.serial==sn, Controller.type==type).first()
+                controller.mode = mode
+                controller.last_conn = int(time.time())
+                db.session.commit()
            
-            if active != ctrl.active:
-                answer.append(json.loads('{"id":0,"operation":"set_active","active": %d}' % ctrl.active))
+                if active != ctrl.active:
+                    messages.append(
+                        dict (
+                            id='0',
+                            operation='set_active',
+                            active=ctrl.active
+                        )
+                    )
                   
-        elif operation == "check_access":
-            card = msg_json.get('card')
-            reader = msg_json.get('reader')
-            print("CHECK ACCESS FROM CONTROLLER %d [%s on %d]" % (sn,card,reader),file=sys.stderr)
-            granted = 1
-            answer.append(json.loads('{"id":%d,"operation":"check_access","granted":%d}' % (req_id,granted)))
+            elif operation == "check_access":
+                card = msg_json.get('card')
+                reader = msg_json.get('reader')
+                print("CHECK ACCESS FROM CONTROLLER %d [%s on %d]" % (sn,card,reader),file=sys.stderr)
+                granted = 1
+                messages.append(
+                    dict(
+                        id=req_id,
+                        operation='check_access',
+                        granted=granted
+                     )
+                )
 
-        elif operation == "events":
-            print("EVENTS FROM CONTROLLER %d" % sn, file=sys.stderr)
-            tz = app.config.get('TZ')
-            event_cnt = 0
-            for event in  msg_json.get('events'):
-                event_cnt += 1
-                ev_time = pytz.timezone(tz).localize(datetime.datetime.strptime(event.get('time'), "%Y-%m-%d %H:%M:%S"))
-                e = Event( event=event.get('event'), flags=event.get('flag'),card = event.get('card'), time=ev_time)
-                db.session.add(e)
-            db.session.commit()
+            elif operation == "events":
+                print("EVENTS FROM CONTROLLER %d" % sn, file=sys.stderr)
+                tz = app.config.get('TZ')
+                event_cnt = len(msg_json.get('events'))
+                for event in msg_json.get('events'):
+                    ev_time = pytz.timezone(tz).localize(datetime.datetime.strptime(event.get('time'), "%Y-%m-%d %H:%M:%S"))
+                    e = Event( event=event.get('event'), flags=event.get('flag'),card = event.get('card'), time=ev_time)
+                    db.session.add(e)
+                db.session.commit()
+                print("EVENT_SUCCESS: %d" % event_cnt, file=sys.stderr)
+                messages.append(
+                    dict(
+                        id = req_id,
+                        operation = 'events',
+                        events_success = event_cnt
+                    )
+                 )
+            else:
+                print('UNKNOWN OERATION',file=sys.stderr)
 
-            print("EVENT_SUCCESS: %d" % event_cnt,file=sys.stderr)
-            answer.append(json.loads('{"id":%d,"operation":"events","events_success":%d}' % (req_id,event_cnt)))
+        for task_jsn in db.session.query(Task.json).filter(Task.serial==sn, Task.type==type):
+            task = json.loads(task_jsn['json'])
+            task['id'] = task_jsn['id']
+            messages.append(task)
 
-        else:
-            print('UNKNOWN OERATION',file=sys.stderr)
-    for task_jsn in db.session.query(Task.json).filter(Task.serial==sn, Task.type==type):
-        if (len(json.dumps(answer))+len(task_jsn['json'])) > 1500:
-            break
-        task = json.loads(task_jsn['json'])
-        task['id'] = task_jsn['id']
-        answer.append(task)
-    answer = '{"date":"%s","interval":%d,"messages":%s}' % (time.strftime("%Y-%m-%d %H:%M:%S"),ctrl.interval, json.dumps(answer))        
-    db.session.close()
-    print ("reponse:"+ answer,file=sys.stderr)
-    return answer
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+        print ("reponse:"+ str (dict ( date=time.strftime("%Y-%m-%d %H:%M:%S"),interval=ctrl.interval, messages = messages )),file=sys.stderr)
+        return json.dumps(
+            dict (
+                date=time.strftime("%Y-%m-%d %H:%M:%S"),
+                interval=ctrl.interval,
+                messages = messages
+            )
+        )
+    except :
+        raise  BadRequest('Genetal error', status_code=400)
+    finally:
+        db.session.close()
+        
